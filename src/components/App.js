f8bc86e7eb5b45746ac0be1e0b625515b8407b69
kpinakula <kpinakoulaki@gmail.com>
2018-06-02 22:05:47 +0100

    Sets up connection with firebase database

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
+import firebaseConfig from '../../secrets'
+import firebase from 'firebase'
 
 class App extends Component {
+  constructor () {
+    super()
+    firebase.initializeApp(firebaseConfig)
+  }
+
   render () {
     return <div>Hi</div>
   }
