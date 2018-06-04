de85ba91335f3790880a6457be587c418ae12219
kpinakula <kpinakoulaki@gmail.com>
2018-06-04 22:52:30 +0100

    Builds out Dashboard to render list of database entries

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-function Dashboard () {
-  return <div>Dashboard</div>
+class Dashboard extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      resources: []
+    }
+  }
+
+  componentDidMount () {
+    const databaseRef = this.props.database.collection('resources')
+    databaseRef.get()
+      .then(snapshot => {
+        const resources = snapshot.docs.map(document => document.data())
+        this.setState({ resources })
+      })
+  }
+
+  renderResources () {
+    return this.state.resources.map((resource, index) => {
+      return <li key={index}>{resource.name}</li>
+    })
+  }
+
+  render () {
+    return (
+      <ul>
+        {this.renderResources()}
+      </ul>
+    )
+  }
+}
+
+Dashboard.propTypes = {
+  database: PropTypes.object.isRequired
 }
 
 export default Dashboard
