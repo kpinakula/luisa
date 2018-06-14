30e25f8cc21d773ab79c41e8634404cf15716cce
kpinakula <kpinakoulaki@gmail.com>
2018-06-14 14:59:54 +0100

    Fixes editor to use uncontrolled component and adds save button

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Controlled as CodeMirror } from 'react-codemirror2'
+import { UnControlled as CodeMirror } from 'react-codemirror2'
 import PropTypes from 'prop-types'
 
 import 'codemirror/lib/codemirror.css'
@@ -10,7 +10,8 @@ class Editor extends Component {
     super(props)
     this.state = {
       originalContent: '',
-      translatedContent: ''
+      translatedContent: '',
+      updatedContent: ''
     }
   }
 
@@ -21,26 +22,62 @@ class Editor extends Component {
         const { originalContent, translatedContent } = doc.data()
         this.setState({
           originalContent,
-          translatedContent
-        })
+          translatedContent})
       })
   }
 
+  applyReadOnly (codeMirror) {
+    const lineCount = codeMirror.lineCount()
+    for (let i = 0; i < lineCount; i++) {
+      const lineContent = codeMirror.getLine(i)
+      const openTagIndices = []
+      const endTagIndices = []
+      for (let x = 0; x < lineContent.length; x++) {
+        if (lineContent[x] === '<') {
+          openTagIndices.push(x)
+        }
+        if (lineContent[x] === '>') {
+          endTagIndices.push(x + 1)
+        }
+      }
+      openTagIndices.map((openTagIndex, index) => {
+        codeMirror.markText({line: i, ch: openTagIndex}, {line: i, ch: endTagIndices[index]}, {
+          readOnly: true,
+          className: 'markup',
+          atomic: true
+        })
+      })
+    }
+  }
+
   render () {
+    if (!this.state.originalContent && !this.state.translatedContent) {
+      return null
+    }
+
     return (
       <div className="editor-container">
+        <button onClick={() => {
+          this.props.database
+            .collection('resources')
+            .doc('test.html')
+            .update({
+              translatedContent: this.state.updatedContent
+            })
+        }}>Save</button>
         <CodeMirror
           className="editor"
-          ref="editor"
           value={this.state.originalContent}
           options={{lineWrapping: true, lineNumbers: true}}
         />
         <CodeMirror
           className="editor"
-          ref="editor"
-          value={this.state.translatedContent}
+          value={this.state.translatedContent || this.state.originalContent}
+          editorDidMount={editor => this.applyReadOnly(editor)}
           options={{lineWrapping: true, lineNumbers: true}}
-          onBeforeChange={(editor, data, value) => this.setState({translatedContent: value})}
+          onChange={(editor, data, value) => {
+            this.setState({updatedContent: value})
+          }}
         />
       </div>
     )
